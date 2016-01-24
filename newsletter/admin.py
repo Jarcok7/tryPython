06401c02cbfae86cbb28d422497a8ae016db09bf
Jarcok7 <jarcok7@gmail.com>
2016-01-24 19:21:14 +0200

    add model SignUp

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import SignUp
+
+class SignUpAdmin(admin.ModelAdmin):
+
+	list_display = ["__unicode__", "full_name", "timestamp"]
+
+	class Meta:
+		model = SignUp	
+
+admin.site.register(SignUp, SignUpAdmin)
